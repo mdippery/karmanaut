28fe1f558167d8d525c3058723cecf37075bac62
Michael Dippery <michael@monkey-robot.com>
2014-05-24 00:14:30 -0700

    Fix date test
    Damn, Java, you ugly!

--- test/karmanaut/utils_test.clj
+++ test/karmanaut/utils_test.clj
@@ -1,11 +1,16 @@
 (ns karmanaut.utils-test
   (:require [clojure.test :refer :all]
-            [karmanaut.utils :refer :all]))
+            [karmanaut.utils :refer :all])
+  (:import [java.text SimpleDateFormat]
+           [java.util TimeZone]))
 
 (deftest test-env
   (is (instance? String (env "HOME" "/Users/mdippery"))))
 
 (deftest test-long-to-date
-  (let [expected "2008-03-31T22:55:26.000-00:00"
-        actual (long-to-date 1207004126)]
-    (is (= expected (str actual)))))
+  (let [expected "2008-03-31T22:55:26.000Z"
+        actual (long-to-date 1207004126)
+        formatter (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")]
+    (doto formatter
+      (.setTimeZone (TimeZone/getTimeZone "UTC")))
+    (is (= expected (.format formatter actual)))))
