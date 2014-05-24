ff710937a1b28fe504b7582e6cf14c0e4605fa02
Michael Dippery <michael@monkey-robot.com>
2014-05-23 23:54:14 -0700

    Fix crawler to work with Reddit API

--- src/karmanaut/crawler.clj
+++ src/karmanaut/crawler.clj
@@ -5,29 +5,27 @@
             [monger.collection :as mc]))
 
 (defn create-user-map [username]
-  ; We're actually making two calls to the Stack Overflow API
+  ; We're actually making two calls to the Reddit API
   ; here -- should find a way to save the data from one call
   ; and re-use it.
   {:username username,
-   :display-name (client/display-name username),
-   :rep (client/rep username)})
+   :link-karma (client/link-karma username),
+   :comment-karma (client/comment-karma username)})
 
 (defn crawl-users [usernames]
   (map create-user-map usernames))
 
 (defn create-user-document [user-map]
-  {:_id (:username user-map), :display_name (:display-name user-map)})
+  {:_id (:username user-map)})
 
 (defn create-sample-document [user-map]
   {:user (:username user-map),
    :timestamp (utils/utcnow),
-   :reputation (:rep user-map)})
+   :link_karma (:link-karma user-map),
+   :comment_karma (:comment-karma user-map)})
 
 (defn update-user! [user-map]
-  (mc/update db/db
-             "users"
-             {:_id (:username user-map)}
-             {:display_name (:display-name user-map)}))
+  (mc/update db/db "users" {:_id (:username user-map)}))
 
 (defn create-sample! [user-map]
   (mc/insert db/db "samples" (create-sample-document user-map)))
--- test/karmanaut/crawler_test.clj
+++ test/karmanaut/crawler_test.clj
@@ -3,14 +3,12 @@
             [karmanaut.crawler :refer :all]))
 
 (def ^{:private true} user-map {:username "mipadi",
-                                :display-name "mipadi",
-                                :rep 100000})
+                                :link-karma 4431,
+                                :comment-karma 26312})
 
 (deftest test-create-user-map
   (let [user-map (create-user-map "mipadi")]
-    (is (= "mipadi" (:username user-map)))
-    (is (= "mipadi" (:display-name user-map)))))
+    (is (= "mipadi" (:username user-map)))))
 
 (deftest test-create-user-document
-  (is (= {:_id "mipadi", :display_name "mipadi"}
-         (create-user-document user-map))))
+  (is (= {:_id "mipadi"} (create-user-document user-map))))
