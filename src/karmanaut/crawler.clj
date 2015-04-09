8b734abeed59acabc8c7c5a1fbb23d9aa220d4e2
Michael Dippery <michael@monkey-robot.com>
2015-04-09 11:52:27 -0700

    Store samples keyed by date
    Although we take several samples per day, we're generally only
    interested in one value per day (although the potential exists to be
    more fine-grained). Therefore, it is prudent to group samples by day,
    storing several samples under one document. Querying the database will
    be much easier this way.
    
    Inspired by [1]. Closes #2.
    
      [1] http://blog.mongodb.org/post/65517193370/schema-design-for-time-series-data-in-mongodb

--- src/karmanaut/crawler.clj
+++ src/karmanaut/crawler.clj
@@ -19,13 +19,18 @@
   {:_id (:username user-map)})
 
 (defn create-sample-document [user-map]
-  {:user (:username user-map),
-   :timestamp (utils/utcnow),
-   :link_karma (:link-karma user-map),
-   :comment_karma (:comment-karma user-map)})
+  (let [mn (utils/midnight (utils/utcnow))
+        secs (utils/seconds-since-midnight (utils/utcnow))
+        stats {(str "link_karma." secs) (:link-karma user-map),
+               (str "comment_karma." secs) (:comment-karma user-map)}]
+    {:user (:username user-map),
+     :timestamp mn,
+     "$set" stats}))
 
 (defn create-sample! [user-map]
-  (mc/insert db/db "samples" (create-sample-document user-map)))
+  (let [q {:user (:username user-map),
+           :timestamp (utils/midnight (utils/utcnow))}]
+    (mc/upsert db/db "samples" q (create-sample-document user-map) {:upsert true})))
 
 (defn insert-samples! [user-maps]
   (dorun (map create-sample! user-maps)))
